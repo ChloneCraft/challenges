936d3f1352d0f5dfbe404fba054a313b700c9847
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-07-12 17:09:41 +0200

    challenge2 finally done

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -2,20 +2,36 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
+const success = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
-
+function hideSuccess() {
+  success.setAttribute("hidden", "");
+}
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
+function showSuccess() {
+  success.removeAttribute("hidden");
+}
+hideTosError();
+hideSuccess();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  const tos = document.getElementById("tos");
+  if (!tos.checked) {
+    showTosError();
+    return;
+  } else {
+    showSuccess();
+  }
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
