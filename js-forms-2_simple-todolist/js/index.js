f507570aad030fead317cab69eb91d1dc4f15918
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-07-13 16:51:16 +0200

    challenge4 done

--- js-forms-2_simple-todolist/js/index.js
+++ js-forms-2_simple-todolist/js/index.js
@@ -2,3 +2,25 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const todoList = document.querySelector('[data-js="todoList"]');
+//task #1
+const headline = document.getElementById("headline");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  form.reset();
+  headline.focus();
+  //task #2
+  createListItem(data);
+});
+
+function createListItem(data) {
+  const newHeadline = document.createElement("h2");
+  newHeadline.textContent = data.headline;
+  const newTask = document.createElement("p");
+  newTask.textContent = data.task;
+  todoList.append(newHeadline);
+  todoList.append(newTask);
+}
