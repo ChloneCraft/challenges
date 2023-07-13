5bf9f9a69adac470e4bbc853343c84fa4e9822bf
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-07-13 16:36:19 +0200

    challenge3 done

--- js-forms-2_simple-live-calculator/js/index.js
+++ js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,14 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+function multiplyFields() {
+  result.innerHTML = firstInput.value * secondInput.value;
+}
+
+firstInput.addEventListener("input", (event) => {
+  multiplyFields();
+});
+secondInput.addEventListener("input", (event) => {
+  multiplyFields();
+});
