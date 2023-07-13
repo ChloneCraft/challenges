b299a88ed87382dda443b9d01a1155166e7ecd7a
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-07-13 16:27:03 +0200

    challenge2 done

--- js-forms-2_controlled-input-uppercase/js/index.js
+++ js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,6 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+input.addEventListener("input", (event) => {
+  event.target.value = event.target.value.toUpperCase();
+});
