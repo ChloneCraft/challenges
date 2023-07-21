cc35da5012274dfc9d3d4e2d6da8eaca17706b8c
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-07-21 16:26:52 +0200

    #2 (first ch) done

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,12 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("response:", response);
+  console.log("data:", data);
+  console.log("R2-D2 eye color:", data.results[2].eye_color);
+}
 
-fetchData();
+await fetchData();
