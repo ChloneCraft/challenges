a62f56b63282bb4430c7d6e9c91ad30d03cb1560
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-03 15:28:43 +0200

    download

--- REACT/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ REACT/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,27 +2,39 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentAPI, setCurrentAPI] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0"
+  );
+  let nextAPI = "";
 
   useEffect(() => {
     async function loadPokemon() {
       try {
-        const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
-        );
+        const response = await fetch(currentAPI);
         const data = await response.json();
+        console.log(data);
         setPokemon(data.results);
+        nextAPI = data.next;
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [currentAPI]);
 
+  function changeAPI() {
+    setCurrentAPI({ nextAPI });
+    console.log({ currentAPI });
+  }
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={changeAPI}>
+        Previous Page
+      </button>
+      <button type="button" onClick={changeAPI}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
